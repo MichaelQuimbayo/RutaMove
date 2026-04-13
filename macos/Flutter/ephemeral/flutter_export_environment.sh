cd4d79d3bdf1b6e4887659352c34460a65ec7cc7
Michael Quimbayo <106043569+MichaelQuimbayo@users.noreply.github.com>
2026-04-13 10:23:30 -0500

    finish apk

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=P:\RutaMove\.fvm\flutter_sdk"
+export "FLUTTER_ROOT=C:\Users\micha\fvm\versions\3.22.2"
 export "FLUTTER_APPLICATION_PATH=P:\RutaMove"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
